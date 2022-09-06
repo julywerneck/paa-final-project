70e562bdeb8dcaff899330a8f469c07f5d2413fc
July Werneck <werneckjuly@gmail.com>
2022-09-05 22:27:10 -0300

    updating function to show the poly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from graphics import *
 
 
-def createPolygon(x1, x2, x3):
+def createPolygon(x1, y1, x2, y2, x3, y3, x4, y4):
     return Polygon(Point(x1, y1), Point(x2, y2),
                    Point(x3, y3), Point(x4, y4))
 
@@ -18,6 +18,7 @@ def main():
     poly = createPolygon(20, 20, 60, 20, 80, 120, 10, 120)
     poly.setFill(color_rgb(255, 0, 255))
     getRepresentations(poly)
+    poly.draw(win)
     win.getMouse()
     win.close()
 
