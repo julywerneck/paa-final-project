3eebc05f8b94273d63e4e52e2dd133677e62d99f
July Werneck <werneckjuly@gmail.com>
2022-09-05 22:25:24 -0300

    updating library used and starting the v1 logic

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-import numpy as np
-import cv2
-
-window = 'Window'
-
-my_img = np.zeros((400, 800, 3), dtype="uint8")
-pts = np.array([[100, 70], [200, 300], [700, 200], [500, 100]], np.int32)
-pts = pts.reshape((-1, 1, 2))
-cv2.polylines(my_img, [pts], True, (0, 255, 255))
+from graphics import *
 
 
-cv2.namedWindow(window, cv2.WINDOW_GUI_EXPANDED)
-cv2.moveWindow(window, 0, 0)
-cv2.setWindowProperty(window, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_GUI_EXPANDED)
-cv2.imshow(window, my_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def createPolygon(x1, x2, x3):
+    return Polygon(Point(x1, y1), Point(x2, y2),
+                   Point(x3, y3), Point(x4, y4))
+
+
+def getRepresentations(poly):
+    topLenght = poly.getPoints()[1].getX() - poly.getPoints()[0].getX()
+    print(topLenght)
+
+
+def main():
+    win = GraphWin("My Window", 500, 500)
+    win.setBackground(color_rgb(0, 0, 0))
+
+    poly = createPolygon(20, 20, 60, 20, 80, 120, 10, 120)
+    poly.setFill(color_rgb(255, 0, 255))
+    getRepresentations(poly)
+    win.getMouse()
+    win.close()
+
+
+main()
