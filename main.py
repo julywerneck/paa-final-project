d99b89962ffe2d484d4673b836dfe9a0b3b111de
ThiiagoAC7 <thiiagooac7@gmail.com>
2022-09-07 22:02:14 -0300

    update fuction to show a rectangle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from graphics import *
+import random
 
 
 def createPolygon(x1, y1, x2, y2, x3, y3, x4, y4):
@@ -10,11 +11,33 @@ def getRepresentations(poly):
     topLenght = poly.getPoints()[1].getX() - poly.getPoints()[0].getX()
     print(topLenght)
 
+'''
+Cria retangulo no centro da tela
+com base na width e hight 
+'''
+def createRectangle(width, height):
+    centerX = width/2 
+    centerY = height/2
+    p1 = Point(centerX-200, centerY+50)
+    p2 = Point(centerX+200, centerY-50)
+    return Rectangle(p1,p2);
 
-def main():
+'''
+TODO :
+-> gerar cor rgb randomica
+'''
+def randomRGBColor():
+    
+    return color_rgb()
+
+def main():    
     win = GraphWin("My Window", 500, 500)
     win.setBackground(color_rgb(0, 0, 0))
 
+    rect = createRectangle(500, 500)
+    rect.setOutline(color_rgb(0,100,0))
+    rect.draw(win)
+    
     poly = createPolygon(20, 20, 60, 20, 80, 120, 10, 120)
     poly.setFill(color_rgb(255, 0, 255))
     getRepresentations(poly)
