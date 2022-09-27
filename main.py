4c9a488da3012f6ae1c5b72cd47a0fa2942d1d19
July Werneck <werneckjuly@gmail.com>
2022-09-26 22:40:45 -0300

    adicionando figuras dentro do retangulo

--- main.py
+++ main.py
@@ -11,33 +11,39 @@ def getRepresentations(poly):
     topLenght = poly.getPoints()[1].getX() - poly.getPoints()[0].getX()
     print(topLenght)
 
+
 '''
 Cria retangulo no centro da tela
-com base na width e hight 
+com base na width e init das peças 
 '''
-def createRectangle(width, height):
-    centerX = width/2 
-    centerY = height/2
-    p1 = Point(centerX-200, centerY+50)
-    p2 = Point(centerX+200, centerY-50)
-    return Rectangle(p1,p2);
+
+
+def createRectangle(width, init):
+    max_height = init + 100
+    max_width = width - 1
+    p1 = Point(max_width, max_height)
+    p2 = Point(1, init)
+    return Rectangle(p1, p2)
+
 
 '''
 TODO :
 -> gerar cor rgb randomica
 '''
+
+
 def randomRGBColor():
-    
     return color_rgb()
 
-def main():    
+
+def main():
     win = GraphWin("My Window", 500, 500)
     win.setBackground(color_rgb(0, 0, 0))
 
-    rect = createRectangle(500, 500)
-    rect.setOutline(color_rgb(0,100,0))
+    rect = createRectangle(500, 20)
+    rect.setOutline(color_rgb(0, 100, 0))
     rect.draw(win)
-    
+
     poly = createPolygon(20, 20, 60, 20, 80, 120, 10, 120)
     poly.setFill(color_rgb(255, 0, 255))
     getRepresentations(poly)
