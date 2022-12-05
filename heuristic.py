04a9a10500fc13c1e27c6bff415749b34c92b2e7
ThiiagoAC7 <thiiagooac7@gmail.com>
2022-12-05 12:36:10 -0300

    fix: exceptions to specific conditions + init heuristic

--- heuristic.py
+++ heuristic.py
@@ -2,12 +2,57 @@ from Tissue import Tissue
 from utils import *
 from Trapezium import *
 
+INC_ESQ = 0
+INC_DIR = 1
+
+'''
+se x3 positivo e x1 > (x2 + x3)
+	\
+senao se x1 < (x2+x3)
+	/
+
+se x3 negativo e (|x3| + x1) > (|x3|+x2)
+	/
+senao se (|x3| + x1) < (|x3|+x2)
+	\
+'''
+def define_inclination(x1,x2,x3):
+    if x3 > 0:
+        if x1 > (x2 + x3):
+            return INC_DIR 
+        elif x1 < (x2 + x3):
+            return INC_ESQ 
+        else:
+            return -1
+    elif x3 < 0:
+        _x3 = abs(x3) 
+        if (_x3 + x1) > (_x3 + x2):
+            return INC_DIR
+        elif (_x3 + x1) < (_x3 + x2):
+            return INC_ESQ
+        else: 
+            return -1 
+    
+
+
+def permutation(p, size, generated_p):
+    if size == 1:
+        return
+        
+    for i in range(size):
+        permutation(p,size-1,generated_p)
+        
+        if size % 2 == 1:
+            p[0], p[size-1] = p[size-1], p[0]
+        else:
+            p[i], p[size-1] = p[size-1], p[i]
+
+
 
 def heuristic(coords,win):
-    tissue = Tissue(len(coords)) 
-    tissue.calc_trapezium(coords[0])
-    tissue.calc_trapezium(coords[1])
-    # tissue.draw_tissue(win)
-    tissue.remove_trapezium()
-    tissue.draw_tissue(win)
+    coords_inc = []
+    for x,i in enumerate(coords):
+        coords_inc.append([define_inclination(float(i[0]),float(i[1]),float(i[2]))])
+        print(f'COORDS = {i}, INC = {coords_inc[x]}')
+    
     return 0
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from graphics import color_rgb
-
+from Color import COLOR
+import sys
 
 TOP_Y = 20
 BOTTOM_Y = 120
@@ -16,10 +17,21 @@ def read_input_from_file(file=""):
         arq = open("input/in.txt")
     coords = []
     lines = [line.strip() for line in arq.readlines()]
-    for i in lines[1:]:
-        coords.append(i.split(' '))
+    n = int(lines[0])
+    for i in range(n):
+        coords.append(lines[i+1].split(' '))
+        verify_conditions(float(coords[i][0]), float(coords[i][1]), float(coords[i][2]))
     arq.close()
     return coords
+
+
+def verify_conditions(x1,x2,x3):
+    if x1 < 0:
+        print(COLOR.FAIL+f'X1 NÃO PODE SER NEGATIVO [coords = {x1},{x2},{x3}]'+COLOR.FAIL)
+        sys.exit()
+    if x3 > x2:
+        print(COLOR.FAIL+f'X3 NÃO PODE SER MAIOR QUE X2 [coords = {x1},{x2},{x3}]'+COLOR.FAIL)
+        sys.exit()
 
 
 def read_input():
